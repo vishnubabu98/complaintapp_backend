b5e0b15fe30bea5fc2938f3655814447de11a011
vishnu <vishnu@NTP-LAP-767.chn.nestgroup.net>
2022-12-11 14:24:43 +0530

    initial

---
+++ src/main/java/com/nest/ComplaintApp_backend/ComplaintAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nest.ComplaintApp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplaintAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplaintAppBackendApplication.class, args);
+	}
+
+}
